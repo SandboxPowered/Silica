be35010d359c83a60c0052a7913abb478a9e810e
Arnaud 'Bluexin' Solé <bluexin.games@gmail.com>
2021-01-02 02:39:24 +0100

    Add Octree for BlockStates

---
+++ common/src/main/kotlin/org/sandboxpowered/silica/artemisExt.kt
@@ -0,0 +1,24 @@
+package org.sandboxpowered.silica
+
+import com.artemis.Component
+import com.artemis.ComponentMapper
+import com.artemis.World
+import com.artemis.WorldConfiguration
+import com.artemis.utils.Bag
+import com.artemis.utils.IntBag
+import net.mostlyoriginal.api.utils.pooling.ObjectPool
+import net.mostlyoriginal.api.utils.pooling.Poolable
+import net.mostlyoriginal.api.utils.pooling.Pools
+
+inline fun <reified T : Component> World.getMapper(): ComponentMapper<T> = this.getMapper(T::class.java)
+
+inline fun <reified T : Poolable> getPool(): ObjectPool<T> = Pools.getPool(T::class.java)
+
+inline fun <reified T> bag(capacity: Int): Bag<T> = Bag(T::class.java, capacity)
+inline fun <reified T> bag(): Bag<T> = Bag(T::class.java)
+
+operator fun <T> Bag<in T>.plusAssign(e: T) = this.add(e)
+
+operator fun IntBag.plusAssign(e: Int) = this.add(e)
+
+inline fun <reified T> WorldConfiguration.registerAs(it: T): WorldConfiguration = this.register(T::class.qualifiedName, it)
---
+++ common/src/main/kotlin/org/sandboxpowered/silica/world/SilicaWorld.kt
@@ -0,0 +1,62 @@
+package org.sandboxpowered.silica.world
+
+import org.sandboxpowered.api.state.BlockState
+import org.sandboxpowered.api.block.Blocks
+import org.sandboxpowered.api.world.BlockFlag
+import org.sandboxpowered.api.block.entity.BlockEntity
+import org.sandboxpowered.api.entity.Entity
+import org.sandboxpowered.api.state.FluidState
+import org.sandboxpowered.api.item.ItemStack
+import org.sandboxpowered.api.shape.Box
+import org.sandboxpowered.api.tags.TagManager
+import org.sandboxpowered.api.util.math.Position
+import org.sandboxpowered.api.world.World
+import org.sandboxpowered.silica.world.util.BlocTree
+import java.util.stream.Stream
+
+abstract class SilicaWorld : World {
+    private val blocks: BlocTree = BlocTree(WORLD_MIN, WORLD_MIN, WORLD_MIN, WORLD_SIZE)
+    private val defaultState = Blocks.AIR.get().baseState
+    private val artemisWorld: com.artemis.World? = null
+
+    override fun getBlockState(position: Position): BlockState {
+        return blocks[position.x, position.y, position.z]
+    }
+
+    override fun setBlockState(position: Position, state: BlockState, vararg flags: BlockFlag): Boolean {
+        blocks[position.x, position.y, position.z] = state
+        return true
+    }
+
+    override fun getBlockEntity(position: Position): BlockEntity? {
+        return null
+    }
+
+    override fun getFluidState(position: Position): FluidState? {
+        return null
+    }
+
+    override fun getEntitiesWithin(box: Box): Stream<Entity> {
+        return Stream.empty()
+    }
+
+    override fun <T : Entity?> getEntitiesWithin(box: Box, filter: Class<T>): Stream<T> {
+        return Stream.empty()
+    }
+
+    override fun getWorldTime(): Long {
+        return 0
+    }
+
+    override fun getTagManager(): TagManager? {
+        return null
+    }
+
+    override fun spawnItem(x: Double, y: Double, z: Double, stack: ItemStack) {}
+
+    private companion object {
+        private const val WORLD_MIN = -33554432
+        private const val WORLD_MAX = 33554431
+        private const val WORLD_SIZE = - WORLD_MIN + WORLD_MAX + 1
+    }
+}
