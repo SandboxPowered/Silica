1029d517295bf6f58b42c9a77c3a769967562e3d
Coded <coded@voxel.games>
2021-01-02 16:17:47 +0000

    Add documentation to state factory

--- common/src/main/java/org/sandboxpowered/silica/state/SilicaStateFactory.java
+++ common/src/main/java/org/sandboxpowered/silica/state/SilicaStateFactory.java
@@ -18,28 +18,32 @@ public class SilicaStateFactory<A, B extends PropertyContainer<B>> implements St
     private final ImmutableSortedMap<String, Property<?>> propertiesByName;
     private final ImmutableList<B> states;
 
-    public SilicaStateFactory(A base, Map<String, Property<?>> map, BiFunction<A, ImmutableMap<Property<?>, Comparable<?>>, B> function) {
+    public SilicaStateFactory(A base, Map<String, Property<?>> map, BiFunction<A, ImmutableMap<Property<?>, Comparable<?>>, B> stateCreator) {
         this.base = base;
         this.propertiesByName = ImmutableSortedMap.copyOf(map);
+
+        // Collect all possible values for set properties
         Stream<List<Pair<Property<?>, Comparable<?>>>> stream = Stream.of(Collections.emptyList());
         for (Map.Entry<String, Property<?>> entry : propertiesByName.entrySet()) {
             stream = stream.flatMap(list -> entry.getValue().getValues().stream().map(comparable -> {
-                List<Pair<Property<?>, Comparable<?>>> list2 = Lists.newArrayList(list);
-                list2.add(Pair.of(entry.getValue(), comparable));
-                return list2;
+                List<Pair<Property<?>, Comparable<?>>> values = Lists.newArrayList(list);
+                values.add(Pair.of(entry.getValue(), comparable));
+                return values;
             }));
         }
 
         Map<Map<Property<?>, Comparable<?>>, B> propertyToState = new LinkedHashMap<>();
         List<B> allStates = new ArrayList<>();
 
+        // Generate combined states with all possible values
         stream.forEach(list -> {
             ImmutableMap<Property<?>, Comparable<?>> propertyValues = list.stream().collect(ImmutableMap.toImmutableMap(Pair::getKey, Pair::getValue));
-            B state = function.apply(base, propertyValues);
+            B state = stateCreator.apply(base, propertyValues);
             propertyToState.put(propertyValues, state);
             allStates.add(state);
         });
 
+        // Initialize the tables within each state allowing them to know other state values
         allStates.forEach(state -> {
             if (state instanceof BaseState) {
                 ((BaseState<A, B>) state).initTable(propertyToState);
