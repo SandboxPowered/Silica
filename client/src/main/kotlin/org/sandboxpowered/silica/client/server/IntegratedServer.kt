85f8f5d29bd2e69ab4f8172dd14548e40f266aaa
Coded <coded@voxel.games>
2021-02-01 15:10:45 +0000

    Add missing method to integrated server

--- client/src/main/kotlin/org/sandboxpowered/silica/client/server/IntegratedServer.kt
+++ client/src/main/kotlin/org/sandboxpowered/silica/client/server/IntegratedServer.kt
@@ -1,10 +1,16 @@
 package org.sandboxpowered.silica.client.server
 
+import akka.actor.typed.ActorRef
 import org.sandboxpowered.silica.StateManager
 import org.sandboxpowered.silica.server.SilicaServer
+import org.sandboxpowered.silica.world.SilicaWorld
 
 class IntegratedServer : SilicaServer() {
     override fun getStateManager(): StateManager {
+        TODO("Not yet implemented")
+    }
+
+    override fun getWorld(): ActorRef<SilicaWorld.Command> {
         TODO("Not yet implemented")
     }
 }
