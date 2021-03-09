f15b496c98e3bd436d9c891b168e6652b8fe8cee
Coded <coded@voxel.games>
2021-03-09 11:58:30 +0000

    Optimise vanilla id mapping to reduce loading times and network sending times

--- common/src/main/kotlin/org/sandboxpowered/silica/StateMappingManager.kt
+++ common/src/main/kotlin/org/sandboxpowered/silica/StateMappingManager.kt
@@ -2,91 +2,99 @@ package org.sandboxpowered.silica
 
 import com.google.gson.Gson
 import com.google.gson.JsonArray
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
 import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap
-import it.unimi.dsi.fastutil.objects.Object2IntArrayMap
-import it.unimi.dsi.fastutil.objects.Object2IntMap
+import it.unimi.dsi.fastutil.objects.*
+import org.apache.logging.log4j.LogManager
 import org.sandboxpowered.api.state.BlockState
 import org.sandboxpowered.api.state.property.Property
 import org.sandboxpowered.silica.registry.SilicaRegistries
 import org.sandboxpowered.silica.state.block.SilicaBlockState
 import org.sandboxpowered.silica.util.getResourceAsString
+import kotlin.math.log
+import kotlin.system.measureTimeMillis
 
 class StateManager {
-    private val rawMap: MutableMap<String, Object2IntMap<String>> = LinkedHashMap()
-    private val stateMap: Object2IntMap<BlockState> = Object2IntArrayMap()
-    private val idMap: Int2ObjectMap<BlockState> = Int2ObjectArrayMap()
+    private val rawMap: Object2ObjectMap<String, Object2IntMap<String>> = Object2ObjectOpenHashMap()
+    private val stateMap: Object2IntMap<BlockState> = Object2IntOpenHashMap()
+    private val idMap: Int2ObjectMap<BlockState> = Int2ObjectOpenHashMap()
+
+    private val logger = LogManager.getLogger()
 
     fun load(): Map<ErrorType, Set<String>> {
-        val string = javaClass.getResourceAsString("/data/silica/states.json")
-        val gson = Gson()
-        val json = gson.fromJson<JsonArray>(string)
-        json.forEach {
-            val obj = it.asJsonObject
-            val id = obj["id"].asString
-            val states = obj["states"].asJsonObject
-            val m = rawMap.computeIfAbsent(id) {
-                val map = Object2IntArrayMap<String>()
-                map.defaultReturnValue(-1)
-                map
+        val errorMap = Object2ObjectOpenHashMap<ErrorType, ObjectOpenHashSet<String>>()
+        measureTimeMillis {
+            val string = javaClass.getResourceAsString("/data/silica/states.json")
+            val gson = Gson()
+            val json = gson.fromJson<JsonArray>(string)
+            json.forEach {
+                val obj = it.asJsonObject
+                val id = obj["id"].asString
+                val states = obj["states"].asJsonObject
+                val m = rawMap.computeIfAbsent(id) {
+                    val map = Object2IntOpenHashMap<String>()
+                    map.defaultReturnValue(-1)
+                    map
+                }
+                states.entrySet().map { entry ->
+                    m.put(entry.key, entry.value.asInt)
+                }
             }
-            states.entrySet().map { entry ->
-                m.put(entry.key, entry.value.asInt)
-            }
-        }
 
-        val errors = HashSet<String>()
+            val errors = ObjectOpenHashSet<String>()
 
-        SilicaRegistries.BLOCK_REGISTRY.internalMap.forEach { (id, block) ->
-            val blockMap = rawMap[id.toString()]
-            if (blockMap == null) {
-                errors.add(id.toString())
-            } else {
-                block.stateFactory.validStates.map { it as SilicaBlockState }.forEach { state ->
-                    val builder = StringBuilder()
-                    state.properties.forEach { entry ->
-                        val value = entry.value as Comparable<Any>
-                        val prop = entry.key as Property<Comparable<Any>>
-                        if (builder.isNotEmpty()) builder.append(",")
-                        builder.append(prop.name).append("@").append(prop.getName(value))
-                    }
+            SilicaRegistries.BLOCK_REGISTRY.internalMap.forEach { (id, block) ->
+                val blockMap = rawMap[id.toString()]
+                if (blockMap == null) {
+                    errors.add(id.toString())
+                } else {
+                    block.stateFactory.validStates.map { it as SilicaBlockState }.forEach { state ->
+                        val builder = StringBuilder()
+                        state.properties.forEach { entry ->
+                            val value = entry.value as Comparable<Any>
+                            val prop = entry.key as Property<Comparable<Any>>
+                            if (builder.isNotEmpty()) builder.append(",")
+                            builder.append(prop.name).append("@").append(prop.getName(value))
+                        }
 
-                    val stateString = builder.toString()
+                        val stateString = builder.toString()
 
-                    val intId = blockMap.getInt(stateString)
+                        val intId = blockMap.getInt(stateString)
 
-                    if (intId == -1) {
-                        errors.add("$id[$builder]")
-                    } else {
-                        stateMap[state] = intId
-                        idMap[intId] = state
-                        blockMap.remove(stateString)
-                        if (blockMap.isEmpty())
-                            rawMap.remove(id.toString())
+                        if (intId == -1) {
+                            errors.add("$id[$builder]")
+                        } else {
+                            stateMap[state] = intId
+                            idMap[intId] = state
+                            blockMap.remove(stateString)
+                            if (blockMap.isEmpty())
+                                rawMap.remove(id.toString())
+                        }
                     }
                 }
             }
-        }
 
-        val missing = HashSet<String>()
+            val missing = ObjectOpenHashSet<String>()
 
-        rawMap.forEach { (block, u) ->
-            run {
-                u.forEach { (state, u) ->
-                    if (state.isEmpty())
-                        missing.add(block)
-                    else
-                        missing.add("$block[$state]")
+            rawMap.forEach { (block, u) ->
+                run {
+                    u.forEach { (state, u) ->
+                        if (state.isEmpty())
+                            missing.add(block)
+                        else
+                            missing.add("$block[$state]")
+                    }
                 }
             }
+            if (errors.isNotEmpty())
+                errorMap[ErrorType.UNKNOWN] = errors
+            if (missing.isNotEmpty())
+                errorMap[ErrorType.MISSING] = missing
+        }.apply {
+            logger.info("Took {}ms to collect vanilla state mappings", this)
         }
-
-        val map = HashMap<ErrorType, Set<String>>()
-        if(errors.isNotEmpty())
-            map[ErrorType.UNKNOWN] = errors
-        if(missing.isNotEmpty())
-            map[ErrorType.MISSING] = missing
-        return map
+        return errorMap
     }
 
     fun toVanillaId(state: BlockState): Int = stateMap.getInt(state)
