8ae19d4e56116b4e507f7ee037374796280c0152
Coded <coded@voxel.games>
2021-03-09 11:57:27 +0000

    Make redstone light block functional

--- content/src/main/java/org/sandboxpowered/silica/minecraft/blocks/RedstoneLightBlock.java
+++ content/src/main/java/org/sandboxpowered/silica/minecraft/blocks/RedstoneLightBlock.java
@@ -2,12 +2,20 @@ package org.sandboxpowered.silica.minecraft.blocks;
 
 import org.sandboxpowered.api.block.BaseBlock;
 import org.sandboxpowered.api.block.Block;
+import org.sandboxpowered.api.ecs.Entity;
+import org.sandboxpowered.api.item.ItemStack;
 import org.sandboxpowered.api.state.BlockState;
 import org.sandboxpowered.api.state.Properties;
 import org.sandboxpowered.api.state.StateFactory;
 import org.sandboxpowered.api.util.Direction;
+import org.sandboxpowered.api.util.Hand;
 import org.sandboxpowered.api.util.math.Position;
+import org.sandboxpowered.api.util.math.Vec3d;
+import org.sandboxpowered.api.world.BlockFlag;
 import org.sandboxpowered.api.world.World;
+import org.sandboxpowered.api.world.WorldReader;
+
+import java.util.Random;
 
 public class RedstoneLightBlock extends BaseBlock {
     public RedstoneLightBlock(Settings settings) {
@@ -18,5 +26,28 @@ public class RedstoneLightBlock extends BaseBlock {
     public void appendProperties(StateFactory.Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
         builder.add(Properties.LIT);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(WorldReader reader, Position pos, Entity player, Hand hand, ItemStack stack, Direction side, Vec3d hitPos) {
+        return getBaseState().with(Properties.LIT, reader.hasNeighborSignal(pos));
+    }
+
+    @Override
+    public void scheduledTick(World serverWorld, Position position, BlockState blockState, Random random) {
+        if (blockState.get(Properties.LIT) && !serverWorld.hasNeighborSignal(position)) {
+            serverWorld.setBlockState(position, blockState.cycle(Properties.LIT), BlockFlag.SEND_TO_CLIENT);
+        }
+    }
+
+    @Override
+    public void onNeighborChanged(BlockState state, World world, Position position, Block other, Position otherPosition) {
+        if (world.isServer()) {
+            boolean lit = state.get(Properties.LIT);
+            if (lit != world.hasNeighborSignal(position)) {
+                if (lit) world.scheduleTick(position, this, 4);
+                else world.setBlockState(position, state.cycle(Properties.LIT));
+            }
+        }
     }
 }
