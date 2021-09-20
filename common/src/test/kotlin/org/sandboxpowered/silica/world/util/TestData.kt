15f3f1692f249dd15305b106ef12acb38a2fa528
Coded <coded@voxel.games>
2021-09-20 02:07:18 +0100

    Mock block identifier

--- common/src/test/kotlin/org/sandboxpowered/silica/world/util/TestData.kt
+++ common/src/test/kotlin/org/sandboxpowered/silica/world/util/TestData.kt
@@ -5,16 +5,13 @@ import io.mockk.every
 import io.mockk.mockk
 import org.sandboxpowered.silica.block.Block
 import org.sandboxpowered.silica.state.block.BlockState
+import org.sandboxpowered.silica.util.Identifier
 
 object TestData {
-
     fun block(name: String, isAir: Boolean = false): Block = mockk(name, relaxed = true) {
+        every { identifier } returns Identifier.of(name)
         every { isAir(any()) } returns isAir
     }
 
-    fun state(blockName: String, isAir: Boolean = false) =
-        BlockState(
-            block(blockName, isAir),
-            ImmutableMap.of()
-        )
+    fun state(blockName: String, isAir: Boolean = false) = BlockState(block(blockName, isAir), ImmutableMap.of())
 }
