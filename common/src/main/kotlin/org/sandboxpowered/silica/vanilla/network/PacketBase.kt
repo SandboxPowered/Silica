3f5402001921ee8cb1ed8e3393f8f838a7f996e2
Coded <coded@voxel.games>
2021-09-24 14:37:35 +0100

    Remove deprecated replacewith

--- common/src/main/kotlin/org/sandboxpowered/silica/vanilla/network/PacketBase.kt
+++ common/src/main/kotlin/org/sandboxpowered/silica/vanilla/network/PacketBase.kt
@@ -1,7 +1,7 @@
 package org.sandboxpowered.silica.vanilla.network
 
 interface PacketBase {
-    @Deprecated("use PacketByteBuf constructor instead", ReplaceWith("Unit"))
-    fun read(buf: PacketByteBuf) = Unit
+    @Deprecated("use PacketByteBuf constructor instead")
+    fun read(buf: PacketByteBuf) {}
     fun write(buf: PacketByteBuf)
 }
